b49783af276e763026ae7269d67048c4b5369fed
Basant kumar Hota <basahota@users.noreply.github.com>
2020-10-20 20:42:57 +0530

    #code

---
+++ src/main/java/com/javatechie/redis/SpringDataRedisExampleApplication.java
@@ -0,0 +1,43 @@
+package com.javatechie.redis;
+
+import com.javatechie.redis.entity.Product;
+import com.javatechie.redis.respository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/product")
+public class SpringDataRedisExampleApplication {
+    @Autowired
+    private ProductDao dao;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+    @DeleteMapping("/{id}")
+    public String remove(@PathVariable int id)   {
+    	return dao.deleteProduct(id);
+	}
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisExampleApplication.class, args);
+    }
+
+}
